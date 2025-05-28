4cbf6bdff48201452dd6a8d15d548b99d26caea6
Max Vogel <max.vogel2@student.kit.edu>
2025-05-28 08:37:04 +0200

    Adapt logging, increase device limit, increase timesteps

--- src/ext/mqt_predictor/train.py
+++ src/ext/mqt_predictor/train.py
@@ -1,28 +1,28 @@
-import logging
 from datetime import datetime
 
 import mqt.bench
 import mqt.predictor
 
-devices = ["ibm_washington", "ibm_montreal"]  # limit devices only for testing
+devices = mqt.bench.devices.get_available_device_names()[:4]  # limit devices only for testing
 
 
 def train_rl_model():
+    print("--- (step 1/3) training reinforcement learning model ---")
+    print_timestamp()
+
     for index, device in enumerate(devices):
-        print(f"--- device {index + 1} of {len(devices)} started ---")
+        print(f"--- (device {index + 1}/{len(devices)}) training {device} ---")
         print_timestamp()
 
-        rl_pred = mqt.predictor.rl.Predictor(
-            figure_of_merit="expected_fidelity", device_name=device, logger_level=logging.DEBUG
-        )  # show debug logs only for testing
-        rl_pred.train_model(timesteps=2048)
-        # timesteps=2048 only for testing
+        rl_pred = mqt.predictor.rl.Predictor(figure_of_merit="expected_fidelity", device_name=device)
+        rl_pred.train_model(timesteps=5 * 2048)  # timesteps=100_000 for full training
 
 
 def train_ml_model():
-    ml_pred = mqt.predictor.ml.Predictor(
-        figure_of_merit="expected_fidelity", devices=devices, logger_level=logging.DEBUG
-    )  # show debug logs only for testing
+    print("--- (step 2/3) generating training data for supervised machine learning ---")
+    print_timestamp()
+
+    ml_pred = mqt.predictor.ml.Predictor(figure_of_merit="expected_fidelity", devices=devices)
     ml_pred.generate_compiled_circuits(timeout=600)  # timeout in seconds
     training_data, name_list, scores_list = ml_pred.generate_trainingdata_from_qasm_files()
     ml_pred.save_training_data(
@@ -30,6 +30,10 @@ def train_ml_model():
         name_list,
         scores_list,
     )
+
+    print("--- (step 3/3) training supervised machine learning model ---")
+    print_timestamp()
+
     ml_pred.train_random_forest_classifier()
 
 
@@ -39,13 +43,10 @@ def print_timestamp():
 
 if __name__ == '__main__':
     start_time = datetime.now()
-
-    print("--- training of reinforcement learning model started ---")
+    print("--- training started ---")
     print_timestamp()
+
     train_rl_model()
-
-    print("--- training of supervised machine learning model started ---")
-    print_timestamp()
     train_ml_model()
 
     print("--- training ended ---")
